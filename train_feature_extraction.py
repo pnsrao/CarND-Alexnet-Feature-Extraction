cb1cb4083bf6f36facf6dad42964630db5680a9f
Subbu Rao <tech@subbu.me>
2017-11-08 17:10:06 -0800

    commit

--- train_feature_extraction.py
+++ train_feature_extraction.py
@@ -2,24 +2,96 @@ import pickle
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from alexnet import AlexNet
+import pandas as pd
+from sklearn.utils import shuffle
 
 # TODO: Load traffic signs data.
 
 # TODO: Split data into training and validation sets.
+# Load pickled data
+import pickle
+# TODO: Fill this in based on where you saved the training and testing data
+
+training_file = './train.p'
+
+with open(training_file, mode='rb') as f:
+    data = pickle.load(f)
+X_train, X_val, y_train, y_val = train_test_split(data['features'], data['labels'], test_size=0.33, random_state=0)
+    
 
 # TODO: Define placeholders and resize operation.
+sign_names = pd.read_csv('signnames.csv')
+nb_classes = 43
+
+x = tf.placeholder(tf.float32, (None, 32, 32, 3))
+y = tf.placeholder(tf.int32, (None))
+one_hot_y = tf.one_hot(y, nb_classes)
+resized = tf.image.resize_images(x, (227, 227))
 
 # TODO: pass placeholder as first argument to `AlexNet`.
-fc7 = AlexNet(..., feature_extract=True)
+fc7 = AlexNet(resized, feature_extract=True)
 # NOTE: `tf.stop_gradient` prevents the gradient from flowing backwards
 # past this point, keeping the weights before and up to `fc7` frozen.
 # This also makes training faster, less work to do!
 fc7 = tf.stop_gradient(fc7)
 
 # TODO: Add the final layer for traffic sign classification.
+shape = (fc7.get_shape().as_list()[-1], nb_classes)  # use this shape for the weight matrix
+# fc8, 1000
+fc8W = tf.Variable(tf.truncated_normal(shape,0,0.1))
+fc8b = tf.Variable(tf.zeros(nb_classes))
 
+logits = tf.matmul(fc7, fc8W) + fc8b
+#probs = tf.nn.softmax(logits)
 # TODO: Define loss, training, accuracy operations.
 # HINT: Look back at your traffic signs project solution, you may
 # be able to reuse some the code.
+cross_entropy = tf.nn.softmax_cross_entropy_with_logits(labels=one_hot_y, logits=logits)
+loss_operation = tf.reduce_mean(cross_entropy)
+# Loss function using L2 Regularization
+#regularizer = tf.nn.l2_loss(weights)
+rate = 0.001 
+optimizer = tf.train.AdamOptimizer(learning_rate = rate)
+training_operation = optimizer.minimize(loss_operation)
 
-# TODO: Train and evaluate the feature extraction model.
+#Evaluation
+correct_prediction = tf.equal(tf.argmax(logits, 1), tf.argmax(one_hot_y, 1))
+accuracy_operation = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+
+BATCH_SIZE = 128
+EPOCHS = 10
+def evaluate(X_data, y_data):
+    num_examples = len(X_data)
+    total_accuracy = 0
+    sess = tf.get_default_session()
+    for offset in range(0, num_examples, BATCH_SIZE):
+        batch_x, batch_y = X_data[offset:offset+BATCH_SIZE], y_data[offset:offset+BATCH_SIZE]
+        accuracy = sess.run(accuracy_operation, feed_dict={x: batch_x, y: batch_y})
+        total_accuracy += (accuracy * len(batch_x))
+    return total_accuracy / num_examples# TODO: Train and evaluate the feature extraction model.
+
+from sklearn.utils import shuffle
+### Train your model here.
+### Calculate and report the accuracy on the training and validation set.
+### Once a final model architecture is selected, 
+### the accuracy on the test set should be calculated and reported as well.
+### Feel free to use as many code cells as needed.
+with tf.Session() as sess:
+    sess.run(tf.global_variables_initializer())
+    num_examples = len(X_train)
+    
+    print("Training...")
+    print('num_examples = ', num_examples)
+    for i in range(EPOCHS):
+        X_train1, y_train1 = shuffle(X_train, y_train)
+        for offset in range(0, num_examples, BATCH_SIZE):
+            end = offset + BATCH_SIZE
+            batch_x, batch_y = X_train1[offset:end], y_train1[offset:end]
+            sess.run(training_operation, feed_dict={x: batch_x, y: batch_y})
+        print("EPOCH {} ...".format(i+1))                    
+        validation_accuracy = evaluate(X_val, y_val)
+        print("Validation Accuracy = {:.3f}".format(validation_accuracy))
+        training_accuracy = evaluate(X_train, y_train)
+        print("Training Accuracy = {:.3f}".format(training_accuracy))
+        print()
+        
